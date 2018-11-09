96bf6ec860bb78af9cc9ed6bd480d7d153ae6e77
Minsuk Eom <boxresin@gmail.com>
2018-11-09 22:17:12 +0900

    메인 액티비티 > 툴바 추가

--- app/src/main/java/winapi251/app/schoolmeal/ui/main/MainActivity.kt
+++ app/src/main/java/winapi251/app/schoolmeal/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package winapi251.app.schoolmeal.ui.main
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import winapi251.app.schoolmeal.R
 
 /** 메인 액티비티 */
@@ -12,5 +14,19 @@ class MainActivity : AppCompatActivity()
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		// 툴바 초기화
+		setSupportActionBar(toolbar)
+
+		// TODO 임시 코드
+		title = "선린인터넷고등학교"
+		toolbar.subtitle = "2018년 11월"
+	}
+
+	/** 옵션 메뉴를 생성해야 할 때 호출된다. */
+	override fun onCreateOptionsMenu(menu: Menu): Boolean
+	{
+		menuInflater.inflate(R.menu.activity_main, menu)
+		return true
 	}
 }
