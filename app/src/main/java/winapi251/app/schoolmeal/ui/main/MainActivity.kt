120acbecd2a6087eeca0f01bede21ba0074f448c
Minsuk Eom <boxresin@gmail.com>
2018-11-10 23:23:59 +0900

    메인 액티비티 > 달력 옆으로 넘기는 동작과 툴바 부제목 날짜 텍스트 연동

--- app/src/main/java/winapi251/app/schoolmeal/ui/main/MainActivity.kt
+++ app/src/main/java/winapi251/app/schoolmeal/ui/main/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.TooltipCompat
+import com.github.sundeepk.compactcalendarview.CompactCalendarView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.card_meal.*
 import winapi251.app.schoolmeal.R
+import java.util.*
+import java.util.Calendar.MONTH
+import java.util.Calendar.YEAR
 
 /** 메인 액티비티 */
 class MainActivity : AppCompatActivity()
@@ -20,6 +24,26 @@ class MainActivity : AppCompatActivity()
 		// 툴바 초기화
 		setSupportActionBar(toolbar)
 
+		// 초기 날짜 텍스트 설정
+		val today = Calendar.getInstance()
+		setToolbarDateText(today[YEAR], today[MONTH] + 1)
+
+		// 날짜 선택 뷰 초기화
+		date_picker.setListener(object: CompactCalendarView.CompactCalendarViewListener {
+			override fun onDayClick(dateClicked: Date)
+			{
+			}
+
+			/** 달력을 옆으로 넘길 때마다 호출된다. */
+			override fun onMonthScroll(firstDayOfNewMonth: Date)
+			{
+				val calendar = Calendar.getInstance().apply { time = firstDayOfNewMonth }
+
+				// 툴바 부제목 날짜 텍스트 변경
+				setToolbarDateText(calendar[YEAR], calendar[MONTH] + 1)
+			}
+		})
+
 		// 툴팁 달기
 		TooltipCompat.setTooltipText(btn_show_meal, getString(R.string.action_show_meal))
 		TooltipCompat.setTooltipText(btn_show_nutrient_info, getString(R.string.action_show_nutrient_info))
@@ -29,7 +53,6 @@ class MainActivity : AppCompatActivity()
 
 		// TODO 임시 코드
 		title = "선린인터넷고등학교"
-		toolbar.subtitle = "2018년 11월"
 	}
 
 	/** 옵션 메뉴를 생성해야 할 때 호출된다. */
@@ -37,5 +60,11 @@ class MainActivity : AppCompatActivity()
 	{
 		menuInflater.inflate(R.menu.activity_main, menu)
 		return true
+	}
+
+	/** 툴바 부제목의 날짜 텍스트를 설정한다. */
+	private fun setToolbarDateText(year: Int, month: Int)
+	{
+		toolbar.subtitle = "${year}년 ${month}월"
 	}
 }
